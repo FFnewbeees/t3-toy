d16f3655396fc60e4054a916daa7575d0ea03fd4
victor <victorliu1999@gmail.com>
2023-05-15 14:18:04 +1000

    [feat]: next image tag

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,6 @@ await import("./src/env.mjs");
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
-
   /**
    * If you have `experimental: { appDir: true }` set, then you must comment the below `i18n` config
    * out.
@@ -17,6 +16,10 @@ const config = {
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
+  },
+
+  images: {
+    domains:['images.clerk.dev']
   },
 };
 export default config;
