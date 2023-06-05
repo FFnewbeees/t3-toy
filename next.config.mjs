016f6ebe1b936926b1ea19415887c30295447ed1
victor <victorliu1999@gmail.com>
2023-06-05 22:54:00 +1000

    [fix]: ignore build errors on vercel, swcminify

--- next.config.mjs
+++ next.config.mjs
@@ -21,5 +21,15 @@ const config = {
   images: {
     domains:['images.clerk.dev']
   },
+
+  // faster build speed 
+  typescript:{
+    ignoreBuildErrors: true
+  },
+
+  eslint:{
+    ignoreDuringBuilds: true
+  },
+  swcMinify: true
 };
 export default config;
